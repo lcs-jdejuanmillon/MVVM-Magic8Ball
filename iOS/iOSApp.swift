be0168a3a92c6d6d24d81eef4206449858086c27
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2023-01-29 16:39:01 -0500

    Initial Commit

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Jacobo de Juan Millon on 2023-01-29.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
