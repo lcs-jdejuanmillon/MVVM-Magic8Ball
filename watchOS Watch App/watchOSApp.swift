b1e373fd855b3bbda88a6d28a890ca50ad876be3
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2023-01-29 17:41:37 -0500

    Added watchOS

---
+++ watchOS Watch App/watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSApp.swift
+//  watchOS Watch App
+//
+//  Created by Jacobo de Juan Millon on 2023-01-29.
+//
+
+import SwiftUI
+
+@main
+struct watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
